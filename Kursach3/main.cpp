293608f122b9f8c159084024884f1521352c2141
svtrev <revtsova_st@edu.surgu.ru>
2023-12-16 22:40:07 +0500

    init

---
+++ Kursach3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Kursach3
+//
+//  Created by Света Ревцова on 16.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
