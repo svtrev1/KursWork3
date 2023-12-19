9a85a6b9cc6479181c0b0bc8a3cbdbf99275470a
svtrev <revtsova_st@edu.surgu.ru>
2023-12-20 01:23:27 +0500

    fix removeItem and searchItem

--- Kursach3/main.cpp
+++ Kursach3/main.cpp
@@ -118,32 +118,46 @@ public:
 
     void removeItem(K key) // Удаление элемента по ключу
     {
-        int index = hashFunction(key);
-        for (auto it = table[index].begin(); it != table[index].end(); ++it) {
-            if (it->first == key) {
-                table[index].erase(it);
-                numElements--;
-                break;
+        int index = hashFunction(key); // Получаем хеш-индекс для ключа
+
+            int startIndex = index; // Сохраняем исходный индекс для контроля цикла
+
+            // Начинаем поиск элемента для удаления
+            while (!table[index].empty()) {
+                if (table[index].front().first == key) { // Если находим ключ, удаляем его
+                    table[index].pop_front(); // Удаляем элемент из списка
+                    numElements--; // Уменьшаем количество элементов
+                    return;
+                }
+                index = (index + 1) % tableSize; // Производим линейное пробирование
+                if (index == startIndex) { // Если мы вернулись к исходному индексу, значит элемент отсутствует
+                    break;
+                }
             }
-        }
+
 
     }
     
     void searchItem(K key) // Нахождение элемента по ключу
     {
-        bool check = true;
         int index = hashFunction(key);
-        for (const auto& pair : table[index])
+        int startIndex = index;
+        while (!table[index].empty())
         {
-            if (pair.first == key)
+            if (table[index].front().first == key)
             {
-                check = false;
-                cout << pair.second; // Если нашелся, выводит значение ключа
-                break;
+                cout << table[index].front().second;
+                return;
+            }
+            index = (index + 1) % tableSize;
+            if (index == startIndex)
+            {
+                cout << "Key not found";
+                return;
             }
         }
-        if (check)
-            cout << "Key not found"; // Если не нашелся
+        cout << "Key not found";
+        return;
     }
     
     void printTable() // Вывод словаря
@@ -182,12 +196,13 @@ public:
 
     void printDictionary()
     {
-        hashTable.printTable();
+//        hashTable.printTable();
+        hashTable.printingHashTable();
     }
     
     void printing()
     {
-        hashTable.printingTable();
+        hashTable.printingHashTable();
         
     }
 
@@ -196,61 +211,61 @@ public:
 
 int main()
 {
-//    Dictionary<int, string> IntString;
-//
-//    IntString.insertItem(1, "A");
-//    IntString.insertItem(2, "B");
-//    IntString.insertItem(3, "C");
-//    IntString.insertItem(4, "D");
-//    IntString.insertItem(2, "B2");
-//    IntString.printDictionary();
-//    IntString.insertItem(6, "Lol");
-//    IntString.printDictionary();
-//    IntString.insertItem(7, "Pop");
-//    IntString.printDictionary();
-//    IntString.insertItem(8, "Pup");
-//    IntString.printDictionary();
-//    IntString.insertItem(91, "Love");
-//    IntString.printDictionary();
-//    IntString.insertItem(88, "88");
-//    IntString.printDictionary();
-//    cout << "Element with key 3: ";
-//    IntString.searchItem(3);
-//    cout << endl;
-//    cout << "Element with key 10: ";
-//    IntString.searchItem(10);
-//    cout << endl;
-//    cout << "Element with key 13: ";
-//    IntString.searchItem(91);
-//    cout << endl;
+    Dictionary<int, string> IntString;
+
+    IntString.insertItem(1, "A");
+    IntString.insertItem(2, "B");
+    IntString.insertItem(3, "C");
+    IntString.insertItem(4, "D");
+    IntString.insertItem(2, "B2");
+    IntString.printDictionary();
+    IntString.insertItem(6, "Lol");
+    IntString.printDictionary();
+    IntString.insertItem(7, "Pop");
+    IntString.printDictionary();
+    IntString.insertItem(8, "Pup");
+    IntString.printDictionary();
+    IntString.insertItem(91, "Love");
+    IntString.printDictionary();
+    IntString.insertItem(88, "88");
+    IntString.printDictionary();
+    cout << "Element with key 3: ";
+    IntString.searchItem(3);
+    cout << endl;
+    cout << "Element with key 10: ";
+    IntString.searchItem(10);
+    cout << endl;
+    cout << "Element with key 13: ";
+    IntString.searchItem(91);
+    cout << endl;
     
-//    Dictionary<int, char> IntChar;
-//
-//    IntChar.insertItem(1, 'a');
-//    IntChar.insertItem(2, 'b');
-//    IntChar.insertItem(3, 'c');
-//    IntChar.insertItem(4, 'd');
-//    IntChar.insertItem(2, 'e');
-//    IntChar.printDictionary();
-//    IntChar.insertItem(6, 'L');
-//    IntChar.printDictionary();
-//    IntChar.insertItem(7, 'L');
-//    IntChar.printDictionary();
-//    IntChar.insertItem(8, 'l');
-//    IntChar.printDictionary();
-//    IntChar.insertItem(91, 'o');
-//    IntChar.printDictionary();
-//    IntChar.insertItem(88, 'v');
-//    IntChar.printDictionary();
-//    cout << "Element with key 3: ";
-//    IntChar.searchItem(3);
-//    cout << endl;
-//    cout << "Element with key 10: ";
-//    IntChar.searchItem(10);
-//    cout << endl;
-//    cout << "Element with key 13: ";
-//    IntChar.searchItem(91);
-//    cout << endl;
+    Dictionary<int, char> IntChar;
+
+    IntChar.insertItem(1, 'a');
+    IntChar.insertItem(2, 'b');
+    IntChar.insertItem(3, 'c');
+    IntChar.insertItem(4, 'd');
+    IntChar.insertItem(2, 'e');
+    IntChar.printDictionary();
+    IntChar.insertItem(6, 'L');
+    IntChar.printDictionary();
+    IntChar.insertItem(7, 'L');
+    IntChar.printDictionary();
+    IntChar.insertItem(8, 'l');
+    IntChar.printDictionary();
+    IntChar.insertItem(91, 'o');
+    IntChar.printDictionary();
+    IntChar.insertItem(88, 'v');
+    IntChar.printDictionary();
+    cout << "Element with key 3: ";
+    IntChar.searchItem(3);
+    cout << endl;
+    cout << "Element with key 10: ";
+    IntChar.searchItem(10);
+    cout << endl;
+    cout << "Element with key 13: ";
+    IntChar.searchItem(91);
+    cout << endl;
     
     Dictionary<char, int> CharInt;
 
@@ -285,123 +300,123 @@ int main()
     cout << endl;
     
     
-//    IntString.printing();
-//    cout << "Element with key 3: ";
-//    IntString.searchItem(8);
-//    cout << endl;
-//    IntString.removeItem(8);
-//    IntString.printDictionary();
-//    cout << "IntString before remove:" << endl;
-//    IntString.printDictionary();
-//
-//    cout << endl;
-//
-//    cout << "Element with key 89: ";
-//    IntString.searchItem(89);
-//    cout << endl;
-//
-//    cout << "Element with key 3: ";
-//    IntString.searchItem(3);
-//    cout << endl;
-//    IntString.removeItem(3);
-//
-//    cout << endl;
-//
-//    cout << "IntString after remove:" << endl;
-//    IntString.printDictionary();
-//
-//    cout << endl;
-//    cout << endl;
-//    cout << endl;
+    IntString.printing();
+    cout << "Element with key 3: ";
+    IntString.searchItem(8);
+    cout << endl;
+    IntString.removeItem(8);
+    IntString.printDictionary();
+    cout << "IntString before remove:" << endl;
+    IntString.printDictionary();
+
+    cout << endl;
+
+    cout << "Element with key 89: ";
+    IntString.searchItem(89);
+    cout << endl;
+
+    cout << "Element with key 3: ";
+    IntString.searchItem(3);
+    cout << endl;
+    IntString.removeItem(3);
+
+    cout << endl;
+
+    cout << "IntString after remove:" << endl;
+    IntString.printDictionary();
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
     
     
     
-//    Dictionary<double, double> DoubleDouble;
-//
-//    DoubleDouble.insertItem(1.1, 3.43);
-//    DoubleDouble.insertItem(2.12, 10.28);
-//    DoubleDouble.insertItem(3.13, 5.00);
-//    DoubleDouble.insertItem(4.15, 4.63);
-//
-//    cout << "IntDouble before remove:" << endl;
-//    DoubleDouble.printDictionary();
-//
-//    cout << endl;
-//
-//    cout << "Element with key 78: ";
-//    DoubleDouble.searchItem(78);
-//    cout << endl;
-//    cout << "Element with key 3.13: ";
-//    DoubleDouble.searchItem(3.13);
-//    cout << endl;
-//    DoubleDouble.removeItem(3.13);
-//
-//    cout << endl;
-//
-//    cout << "IntDouble after remove:" << endl;
-//    DoubleDouble.printDictionary();
-//
-//    cout << endl;
-//    cout << endl;
-//    cout << endl;
-//
-//    Dictionary<string, double> StringDouble;
-//
-//    StringDouble.insertItem("Владимир", 3.27);
-//    StringDouble.insertItem("Родион", 10.28);
-//    StringDouble.insertItem("Евгений", 5.00);
-//    StringDouble.insertItem("Илоночка", 4.63);
-//
-//    cout << "StringDouble before remove:" << endl;
-//    StringDouble.printDictionary();
-//
-//    cout << endl;
-//
-//    cout << "Element with key Лолошка: ";
-//    StringDouble.searchItem("Лолошка");
-//    cout << endl;
-//    cout << "Element with key Евгений: ";
-//    StringDouble.searchItem("Евгений");
-//    cout << endl;
-//    StringDouble.removeItem("Евгений");
-//
-//    cout << endl;
-//
-//    cout << "StringDouble after remove:" << endl;
-//    StringDouble.printDictionary();
-//
-//    cout << endl;
-//    cout << endl;
-//    cout << endl;
-//
-//    Dictionary<string, string> StringString;
-//
-//    StringString.insertItem("Привет", "Hello");
-//    StringString.insertItem("Пока", "By");
-//    StringString.insertItem("Как дела?", "How are you?");
-//    StringString.insertItem("Сколько тебе лет?", "How old are you?");
-//
-//    cout << "StringString before remove:" << endl;
-//    StringString.printDictionary();
-//
-//    cout << endl;
-//
-//    cout << "Element with key Нет, Value: ";
-//    StringString.searchItem("Нет");
-//    cout << endl;
-//    cout << "Element with key Пока, Value: ";
-//    StringString.searchItem("Пока");
-//    cout << endl;
-//    StringString.removeItem("Пока");
-//
-//    cout << endl;
-//
-//    cout << "StringString after remove:" << endl;
-//    StringString.printDictionary();
-//
-//    cout << endl;
-//    cout << endl;
-//    cout << endl;
+    Dictionary<double, double> DoubleDouble;
+
+    DoubleDouble.insertItem(1.1, 3.43);
+    DoubleDouble.insertItem(2.12, 10.28);
+    DoubleDouble.insertItem(3.13, 5.00);
+    DoubleDouble.insertItem(4.15, 4.63);
+
+    cout << "IntDouble before remove:" << endl;
+    DoubleDouble.printDictionary();
+
+    cout << endl;
+
+    cout << "Element with key 78: ";
+    DoubleDouble.searchItem(78);
+    cout << endl;
+    cout << "Element with key 3.13: ";
+    DoubleDouble.searchItem(3.13);
+    cout << endl;
+    DoubleDouble.removeItem(3.13);
+
+    cout << endl;
+
+    cout << "IntDouble after remove:" << endl;
+    DoubleDouble.printDictionary();
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
+
+    Dictionary<string, double> StringDouble;
+
+    StringDouble.insertItem("Владимир", 3.27);
+    StringDouble.insertItem("Родион", 10.28);
+    StringDouble.insertItem("Евгений", 5.00);
+    StringDouble.insertItem("Илоночка", 4.63);
+
+    cout << "StringDouble before remove:" << endl;
+    StringDouble.printDictionary();
+
+    cout << endl;
+
+    cout << "Element with key Лолошка: ";
+    StringDouble.searchItem("Лолошка");
+    cout << endl;
+    cout << "Element with key Евгений: ";
+    StringDouble.searchItem("Евгений");
+    cout << endl;
+    StringDouble.removeItem("Евгений");
+
+    cout << endl;
+
+    cout << "StringDouble after remove:" << endl;
+    StringDouble.printDictionary();
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
+
+    Dictionary<string, string> StringString;
+
+    StringString.insertItem("Привет", "Hello");
+    StringString.insertItem("Пока", "By");
+    StringString.insertItem("Как дела?", "How are you?");
+    StringString.insertItem("Сколько тебе лет?", "How old are you?");
+
+    cout << "StringString before remove:" << endl;
+    StringString.printDictionary();
+
+    cout << endl;
+
+    cout << "Element with key Нет, Value: ";
+    StringString.searchItem("Нет");
+    cout << endl;
+    cout << "Element with key Пока, Value: ";
+    StringString.searchItem("Пока");
+    cout << endl;
+    StringString.removeItem("Пока");
+
+    cout << endl;
+
+    cout << "StringString after remove:" << endl;
+    StringString.printDictionary();
+
+    cout << endl;
+    cout << endl;
+    cout << endl;
     
     return 0;
 }
